ffabb92a4b54b5a7d935c2b17eb394ce8ff42095
JoeRenDev <josephrenwick@outlook.com>
2026-06-28 19:08:21 +0100

    Removed API import

--- faceit-stats/main.py
+++ faceit-stats/main.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify
-import API
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Flask app is running"})
+    return "Flask is working!"
 
 if __name__ == "__main__":
     app.run(debug=True)
