287d64c83542853e71c296d382693602ef5d001d
JoeRenDev <josephrenwick@outlook.com>
2026-07-04 14:39:29 +0100

    Outlined basic logic plan

--- faceit-stats/api.py
+++ faceit-stats/api.py
@@ -12,6 +12,8 @@ url = f"https://open.faceit.com/data/v4/players?nickname={USERNAME}"
 
 response = requests.get(url, headers=headers)
 
+#When main.py will call api.py the API data should be passed to main.py to be used in index.html
+
 print("Status Code:", response.status_code)
 print("JSON Reponse:")
 print(json.dumps(response.json(), indent=4))
--- faceit-stats/main.py
+++ faceit-stats/main.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+  #Data from API method should be used with render_template to populate placeholders in index.html
+
 
 if __name__ == "__main__":
     app.run(debug=True)
